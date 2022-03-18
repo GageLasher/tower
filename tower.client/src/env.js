76cde239cf745bf2a4db8c293fe70c296e3b51b6
Gage Lasher <rsgage21@gmail.com>
2022-03-18 13:37:06 -0600

    commit

--- tower.client/src/env.js
+++ tower.client/src/env.js
@@ -1,6 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-zvvg4bfo.us.auth0.com'
+export const clientId = 'jbRNwzq3cTJtPc71ePMVbKZrhIkqmmIE'
+export const audience = 'https://gage-dev.com'
+
